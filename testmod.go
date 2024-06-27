09abd574b7d2980351403a14029ca2d6eca3eba4
Tesla <anuar.musljumov@gmail.com>
2024-06-27 16:18:42 +0500

    Update module path for version 2.0.0

--- testmod.go
+++ testmod.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 )
 
-// Hi returns a friendly greeting in language lang
 func Hi(name, lang string) (string, error) {
+	if name == "" {
+		return "", errors.New("name cannot be empty")
+	}
+
 	switch lang {
 	case "en":
-		return fmt.Sprintf("Hi, %s!", name), nil
+		return fmt.Sprintf("Hello, %s!", name), nil
 	case "pt":
-		return fmt.Sprintf("Oi, %s!", name), nil
-	case "es":
-		return fmt.Sprintf("¡Hola, %s!", name), nil
-	case "fr":
-		return fmt.Sprintf("Bonjour, %s!", name), nil
+		return fmt.Sprintf("Olá, %s!", name), nil
 	default:
-		return "", errors.New("unknown language")
+		return "", errors.New("unsupported language")
 	}
 }
